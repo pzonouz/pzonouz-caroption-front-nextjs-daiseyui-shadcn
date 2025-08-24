7b8b0bcf046a88a58ddd02df92adc8ac688ccce7
Peyman Khalili <p.zonouz@gmail.com>
2025-08-24 16:01:04 -0400

    fix next.config file image domain for image issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   images: {
+    domains: ["carroption.ir"],
     remotePatterns: [
       {
         protocol: "http",
