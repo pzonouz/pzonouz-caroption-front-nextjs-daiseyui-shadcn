6c97d55354934fcb256068491d4d61ad9fb5a5aa
Peyman Khalili <p.zonouz@gmail.com>
2025-09-01 01:00:02 -0400

    add /bakend/ to disallow urls

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/crm/",
+      disallow: ["/crm/", "/backend/"],
     },
     sitemap: `${process.env.BACKEND_URL}/sitemap.xml`,
   };
