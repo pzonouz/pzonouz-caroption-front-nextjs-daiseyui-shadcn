a4554136371533b16cff2df9498c602c0207d106
peyman khalili zonouz <p.zonouz@gmail.com>
2025-10-26 23:00:27 +0330

    product revlidate to 60 s

--- app/(main)/products/page.tsx
+++ app/(main)/products/page.tsx
@@ -2,7 +2,9 @@ import { Products } from "@/app/components/Products/Products";
 import { Product } from "@/app/lib/schemas";
 
 const page = async () => {
-  const productsRes = await fetch(`${process.env.BACKEND_URL}/products`);
+  const productsRes = await fetch(`${process.env.BACKEND_URL}/products`, {
+    next: { revalidate: 60 },
+  });
   const products: Product[] = await productsRes.json();
   return (
     <div>
