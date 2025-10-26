df278c4d4c8df0fc114c89c38681445e41f78cb2
peyman khalili zonouz <p.zonouz@gmail.com>
2025-10-26 22:54:33 +0330

    latest

--- app/components/Products/Products.tsx
+++ app/components/Products/Products.tsx
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 
 const Products = ({ products }: { products: Product[] }) => (
   <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:mt-14 md:mt-24">
-    {products.map((product) => (
+    {products?.map((product: Product) => (
       <ProductCard key={product.id} product={product} />
     ))}
   </div>
