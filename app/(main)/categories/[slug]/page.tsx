a5438fc54ac92f48f47156fd9ffcbe3c38f38293
peyman khalili zonouz <p.zonouz@gmail.com>
2025-10-31 21:03:07 +0330

    rm classname hvh from produsta pge

--- app/(main)/categories/[slug]/page.tsx
+++ app/(main)/categories/[slug]/page.tsx
@@ -45,7 +45,7 @@ const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
     (article) => article?.showInProducts,
   );
   return (
-    <div className="h-dvh">
+    <div className="">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:mt-14 md:mt-24">
         {products.map((product: Product) => (
           <ProductCard key={product.id} product={product} />
