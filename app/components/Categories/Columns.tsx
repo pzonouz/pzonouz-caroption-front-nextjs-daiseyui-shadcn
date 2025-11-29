ef7e1363a984e018161a2c00e5278c6ebe82a29b
peyman khalili zonouz <p.zonouz@gmail.com>
2025-11-29 23:08:11 +0330

    categories parent sort

--- app/components/Categories/Columns.tsx
+++ app/components/Categories/Columns.tsx
@@ -3,6 +3,8 @@
 import { ColumnDef } from "@tanstack/react-table";
 import CategoryActionsCell from "./CategoryActionsCell";
 import { Category } from "../../lib/schemas";
+import { Button } from "@/components/ui/button";
+import { ArrowUpDown } from "lucide-react";
 
 export const columns: ColumnDef<Category>[] = [
   {
@@ -14,13 +16,31 @@ export const columns: ColumnDef<Category>[] = [
   },
   {
     accessorKey: "parentName",
-    header: () => <div className="text-right">والد</div>,
+    header: ({ column }) => (
+      <div className="text-center">
+        <Button
+          className="cursor-pointer"
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          والد
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      </div>
+    ),
     cell: ({ row }) => {
       return (
         <div className="text-right font-medium">
           {row?.original?.parentName}
         </div>
       );
+    },
+    sortingFn: (rowA, rowB, columnId) => {
+      const a = rowA.getValue<string>(columnId) ?? "";
+      const b = rowB.getValue<string>(columnId) ?? "";
+
+      // مرتب‌سازی با توجه به زبان فارسی
+      return a.localeCompare(b, "fa", { sensitivity: "base" });
     },
   },
   {
